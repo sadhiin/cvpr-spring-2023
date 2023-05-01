6f152d93ac706e4b43cea95259082fc41b7d8eb9
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-05-01 19:57:13 +0600

    few update made

--- Final/Assignment-1/app.py
+++ Final/Assignment-1/app.py
@@ -5,32 +5,28 @@ import pickle
 import numpy as np
 from tensorflow.keras.models import load_model
 
+model = load_model(f"mymodel.h5", compile=False)
 
-model = load_model(f"Final\Assignment-1\mymodel.h5",compile=False)
-
-with open(f'Final\Assignment-1\category2label.pkl', 'rb') as cat:
+with open(f'category2label.pkl', 'rb') as cat:
     catrgory_2_label = pickle.load(cat)
 
 
-print("Categories: ",catrgory_2_label)
+print("Categories: ", catrgory_2_label)
 
 img_size = (100, 100)
 
 box_colors = {0: (0, 0, 255), 1: (0, 255, 0), 2: (0, 255, 255)}
+text_colors = {0: (0, 0, 255), 1: (0, 255, 0), 2: (30, 144, 255)}
 
-face_cascade = cv2.CascadeClassifier(f'Final\Assignment-1\haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 
 # video from webcam
-
 cap = cv2.VideoCapture(0)
 begin_time = time.time()
 frame_cnt = 0
 
 while cap.isOpened():
     rate, frame = cap.read()
-
-    if not rate:
-        break
 
     frame_cnt += 1
     frame = cv2.flip(frame, 1)
@@ -47,18 +43,19 @@ while cap.isOpened():
         pred = model.predict(data)
 
         target = np.argmax(pred, axis=1)[0]
-
+        print("Predection => ", target)
         # drawing box
-        cv2.rectangle(img=frame, pt1=(x, y), pt2=(x+w, y+h),color=box_colors[target], thickness=2)
+        cv2.rectangle(img=frame, pt1=(x, y), pt2=(x+w, y+h),
+                      color=box_colors[target], thickness=2)
         texts = "{}: {:.2f}".format(catrgory_2_label[target], pred[0][target])
         cv2.putText(frame, text=texts, org=(x, y-5), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=0.6, color=(255, 255, 255), thickness=1)
+                    fontScale=0.6, color=(75, 0, 130), thickness=1)
 
     endtime = time.time()-begin_time
     fps = frame_cnt / endtime
 
     cv2.putText(img=frame, text="FPS: "+str(round(fps, 2)), org=(10, 20),
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX,  fontScale=0.5, color=(255, 255, 255), thickness=1)
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,  fontScale=0.5, color=(30, 144, 255), thickness=1)
     # Show the frame
     cv2.imshow('Face Mask Detection', frame)
     # for terminationg ESC key
